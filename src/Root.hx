f8366f0d7cc00bc81c2be0fdc3d2baed7c189cb8
BlackAR <anthony.r.black13@gmail.com>
2015-04-13 14:15:20 -0700

    Added root file
    Did not add a makefile, since everyone will have their own. Added root
    file, src/assets folders.

---
+++ src/Root.hx
@@ -0,0 +1,47 @@
+import starling.display.*;
+import starling.utils.AssetManager;
+import starling.core.Starling;
+import starling.animation.Transitions;
+import starling.textures.TextureSmoothing;
+import starling.textures.Texture;
+import starling.text.TextField;
+import starling.events.*;
+import flash.ui.Keyboard;
+
+class Root extends Sprite {
+
+    public static var assets:AssetManager;
+
+    public function new() {
+        super();
+      
+    }
+    public function initialize(startup:Startup) {
+        assets = new AssetManager();
+        // enqueue here
+        assets.enqueue("assets/loading.png");
+        assets.loadQueue(function onProgress(ratio:Float) {
+            if(ratio == 1) {
+                Starling.juggler.tween(startup.loadingBitmap,
+                    1.0,
+                    {
+                        transition: Transitions.EASE_OUT,
+                        delay: 1.0,
+                        alpha: 0,
+                        onComplete: function()
+                        {
+                            startup.removeChild(startup.loadingBitmap);
+                        }
+                    });
+                //start();
+            }
+        });
+    }
+
+    public function start(){
+        removeChildren(0,-1,true);
+        removeEventListeners();
+        game = new Game();
+        Starling.juggler.add(game);
+        addChild(game);
+    }
