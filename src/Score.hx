af0cfd2db031504b77eb3abdf8364cd34a9231c8
Salvatore Bottiglieri <sb797@nau.edu>
2015-04-17 10:39:00 -0700

    getting score file set up

---
+++ src/Score.hx
@@ -0,0 +1,14 @@
+import starling.display.*;
+import starling.utils.AssetManager;
+import starling.core.Starling;
+import starling.animation.Transitions;
+import starling.textures.TextureSmoothing;
+import starling.textures.Texture;
+import starling.text.TextField;
+import starling.events.*;
+import flash.ui.Keyboard;
+import Game;
+
+class Score extends Sprite{
+
+}
