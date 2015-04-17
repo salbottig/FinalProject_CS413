798af3dde33b520833b79931bd69358c12a658e0
Salvatore Bottiglieri <sb797@nau.edu>
2015-04-17 10:39:59 -0700

    got rid of keyboard import

--- src/Score.hx
+++ src/Score.hx
@@ -6,7 +6,6 @@ import starling.textures.TextureSmoothing;
 import starling.textures.Texture;
 import starling.text.TextField;
 import starling.events.*;
-import flash.ui.Keyboard;
 import Game;
 
 class Score extends Sprite{
