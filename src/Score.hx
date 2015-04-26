0f7b91ad5c50c9e9e8743d62f871990ce410d71e
Salvatore Bottiglieri <sb797@nau.edu>
2015-04-26 13:39:32 -0700

    updated score to be in center. Helps take up more real estate

--- src/Score.hx
+++ src/Score.hx
@@ -9,11 +9,12 @@ import Root;
 class Score extends Sprite{
 	public var score:Int;
 	public var scoreText:TextField;
+	public var tutorialText:TextField;
 	public function new(){
 		super();
 		score = 0;
-		scoreText = new TextField(128, 32, "Score: " + score);
-		scoreText.hAlign = HAlign.LEFT;
+		scoreText = new TextField(635,150, "Score: " + score, "eastwood", 35);
+		scoreText.color = 0x000000;
 		addChild(scoreText);
 		//addEventListener(KeyboardEvent.KEY_DOWN, keyDown); //debugging
 	}
