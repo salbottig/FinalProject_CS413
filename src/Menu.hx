d530041957a70a743594acb9a16e403303c82df8
Veronika Alves <vt77@nau.edu>
2015-04-28 22:06:29 -0700

    Names listed under credits

--- src/Menu.hx
+++ src/Menu.hx
@@ -30,6 +30,8 @@ class Menu extends Sprite{
 	public var creditText:TextField;
 	public var tutorialText:TextField;
 
+	public var instructions:TextField;
+
 
 	public function new() {
 		super();
@@ -49,11 +51,11 @@ class Menu extends Sprite{
 		addChild(titleText);
 
 		creditText = new TextField(300,100, "Credits", "eastwood", 55);
-		creditText.x = 175;
+		creditText.x = 165;
 		creditText.y = 10;
 
 		tutorialText = new TextField(325,100, "How To Play", "eastwood", 55);
-		tutorialText.x = 175;
+		tutorialText.x = 165;
 		tutorialText.y = 10;
 
 		play = new Image(Root.assets.getTexture("play_button"));
@@ -70,6 +72,16 @@ class Menu extends Sprite{
 		tutorial.x = 420;
 		tutorial.y = 325;
 		addChild(tutorial);
+
+		instructions = new TextField(500,500, "
+											   Anthony Black
+											   Christopher Heiser
+											   Erin Bailey
+											   Salvatore Bottiglieri
+											   Tanner Stevens
+											   Veronika Alves","eastwood", 30);
+		instructions.x = 50;
+		instructions.y = 10;
 
 		play.addEventListener(TouchEvent.TOUCH, function(e:TouchEvent){
             var touch = e.getTouch(play, TouchPhase.BEGAN);
@@ -90,6 +102,7 @@ class Menu extends Sprite{
                    	play.y = 500;
                    	tutorial.x = 50;
                    	tutorial.y = 500;
+                   	addChild(instructions);
         }});  
 
         tutorial.addEventListener(TouchEvent.TOUCH, function(e:TouchEvent){
@@ -98,6 +111,7 @@ class Menu extends Sprite{
                    	removeChild(tutorial);
                    	removeChild(creditText);
                    	removeChild(titleText);
+                   	removeChild(instructions);
                    	addChild(tutorialText);
                    	addChild(credits);
                    	play.x = 500;
