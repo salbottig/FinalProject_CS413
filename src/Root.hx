554a5b15bc54aac6d76f7c654563a6a8ba936d51
TannerStevens <tanner.stevens42@gmail.com>
2015-04-30 20:25:38 -0700

    BitmapFont Fix

--- src/Root.hx
+++ src/Root.hx
@@ -4,7 +4,7 @@ import starling.core.Starling;
 import starling.animation.Transitions;
 import starling.textures.TextureSmoothing;
 import starling.textures.Texture;
-import starling.text.TextField;
+import starling.text.*;
 import starling.events.*;
 import flash.ui.Keyboard;
 import flash.media.SoundChannel;
